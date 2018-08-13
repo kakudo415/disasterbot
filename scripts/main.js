dad9fc947c9214db429a5e85b6cdf4d6167e9f70
Kakudo <kakudo415@gmail.com>
2018-08-13 19:11:45 +0900

    logging

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,7 @@
 'use strict';
+let fs = require('fs');
+let log = require('log');
+let logger = new log('debug', fs.createWriteStream('./access_log', 'utf8'));
 
 const verifyToken = process.env.JMA_VERIFY_TOKEN;
 
@@ -7,13 +10,15 @@ module.exports = (robot) => {
     const query = req.query;
     if (query['hub.mode'] !== 'subscribe') {
       res.status(404).send('MODE ERROR').end();
+      logger.error(query);
       return;
     }
     if (query['hub.verify_token'] !== verifyToken) {
       res.status(404).send('VERIFY_TOKEN ERROR').end();
+      logger.error(query);
       return;
     }
-    console.log(query);
     res.status(200).send(query['hub.challenge']).end();
+    logger.info(query);
   });
 };
