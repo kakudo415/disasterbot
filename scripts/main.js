78db12a4629ca34a4ba52a450eaff238a130e689
Kakudo <kakudo415@gmail.com>
2018-08-17 16:30:22 +0900

    feed

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,15 @@
 'use strict';
 let fs = require('fs');
 let log = require('log');
+let feedparser = new require('feedparser');
 let logger = new log('debug', fs.createWriteStream('./access_log', {flags: 'a'}));
 
 const verifyToken = process.env.JMA_VERIFY_TOKEN;
 
 module.exports = (robot) => {
+  robot.hear(/!alertbot/, (msg) => {
+    msg.send('私は気象庁から警報などの情報を取得し、お伝えします');
+  });
   robot.router.get('/sub', (req, res) => {
     const query = req.query;
     if (query['hub.mode'] !== 'subscribe' && query['hub.mode'] !== 'unsubscribe') {
@@ -22,7 +26,20 @@ module.exports = (robot) => {
     logger.info(JSON.stringify(query));
   });
   robot.router.post('/sub', (req, res) => {
+    let feed;
     res.status(200).end();
+    console.log(req.body);
+    req.body.pipe(feedparser);
+    feedparser.on('readable', () => {
+      let buffer;
+      while (buffer = stream.read()) {
+        feed += buffer;
+      }
+    });
+    feedparser.on('end', () => {
+      console.log(feed);
+    });
+    // robot.send({room: 'alert-room'}, );
     logger.info(JSON.stringify(req.body));
   });
 };
