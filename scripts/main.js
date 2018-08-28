a8a1d56f58e672e22e8ed1772419817da9bef570
Kakudo <kakudo415@gmail.com>
2018-08-28 20:58:00 +0900

    震度速報をサポート

--- scripts/main.js
+++ scripts/main.js
@@ -65,16 +65,30 @@ module.exports = (robot) => {
             let date = new Date(Report.Head[0].ReportDateTime[0]);
             let time = `${date.getFullYear()}年 ${date.getMonth() + 1}月 ${date.getDate()}日 ${date.getHours()}時 ${date.getMinutes()}分 ${Report.Head[0].InfoType[0]}`;
             switch (Report.Head[0].InfoKind[0]) {  // 運用種別情報
-              // case '震度速報':
-              //   break;
-              // case '震源速報':
-              //   break;
-              // case '震源要素更新のお知らせ':
-              //   break;
-              // case '地震回数情報':
-              //   break;
-              // case '地震の活動状況等に関する情報':
-              //   break;
+              case '震度速報':
+                let maxInt = Report.Body[0].Intensity[0].Observation[0].MaxInt[0];
+                message += `*震度速報（${time}）*\n` +
+                    `最大震度 : ${maxInt}\n` +
+                    `最大震度を観測した地域 : `;
+                Report.Body[0].Intensity[0].Observation[0].Pref.forEach((pref) => {
+                  if (pref.MaxInt[0] == maxInt) {
+                    message += `${pref.Name[0]} `;
+                  }
+                });
+                break;
+
+                // case '震源速報':
+                //   break;
+
+                // case '震源要素更新のお知らせ':
+                //   break;
+
+                // case '地震回数情報':
+                //   break;
+
+                // case '地震の活動状況等に関する情報':
+                //   break;
+
               case '地震情報':
                 message += `*${Report.Head[0].Title[0]}（${time}）*\n` +
                     `震央地 : ${Report.Body[0].Earthquake[0].Hypocenter[0].Area[0].Name[0]}\n`;
@@ -90,23 +104,29 @@ module.exports = (robot) => {
                 }
                 message += `${Report.Body[0].Comments[0].ForecastComment[0].Text[0]}`;
                 break;
-              // case '津波情報':
-              //   break;
-              // case '津波警報・注意報・予報':
-              //   break;
-              case '東海地震関連情報':
-                return;  // 東海地震関連情報はこのBOTの主旨から外れるので、とりあえずは投稿しない（要望次第）
-              case '火山の状況に関する解説情報':
-                return;  // 火山の状況に関する解説情報はこのBOTの主旨から外れるので、とりあえずは投稿しない（要望次第）
+
+                // case '津波情報':
+                //   break;
+
+                // case '津波警報・注意報・予報':
+                //   break;
+
+                // case '噴火速報':
+                //   break;
+
               case '噴火に関する火山観測報':
                 message += `*${Report.Head[0].InfoKind[0]}（${time}）*\n` +
                     `場所 : ${Report.Body[0].VolcanoInfo[0].Item[0].Areas[0].Area[0].Name[0]} ${Report.Body[0].VolcanoInfo[0].Item[0].Areas[0].Area[0].CraterName[0]}\n` +
                     `現象 : ${Report.Body[0].VolcanoInfo[0].Item[0].Kind[0].Name[0]}`;
                 break;
+
               case '降灰予報':
                 return;  // 降灰予報はこのBOTの主旨から外れるので、とりあえずは投稿しない（要望次第）
-              // case '噴火速報':
-              //   break;
+              case '東海地震関連情報':
+                return;  // 東海地震関連情報はこのBOTの主旨から外れるので、とりあえずは投稿しない（要望次第）
+              case '火山の状況に関する解説情報':
+                return;  // 火山の状況に関する解説情報はこのBOTの主旨から外れるので、とりあえずは投稿しない（要望次第）
+
               default:
                 message += `*${Report.Head[0].Title[0]}*\n${Report.Head[0].Headline[0].Text[0]}`;
             }
