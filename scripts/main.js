832547d787c58c657fa057f0229b6f1e83448be7
Kakudo <kakudo415@gmail.com>
2018-12-21 16:32:57 +0900

    とりあえずヘッドライン出力

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,6 @@
-'use strict';
-const request = require('request');
-
-const alreadyRead = [];
+"use strict";
+const request = require("request");
+const message = require("./message.js");
 
 module.exports = (bot) => {
   setInterval(() => {
@@ -15,10 +14,11 @@ module.exports = (bot) => {
     try {
       const data = JSON.parse(body);
       for (let uuid of data.UUID) {
-        if (!alreadyRead.includes(uuid)) {
-          request.get(`https://kakudo.app/kishow/${uuid}`, info);
-          alreadyRead.push(uuid);
+        if (bot.brain.get(`ALERTBOT:${uuid}`)) {
+          continue;
         }
+        request.get(`https://kakudo.app/kishow/${uuid}`, info);
+        bot.brain.set(`ALERTBOT:${uuid}`, "もうみた");
       }
     } catch (e) {
       console.error(e);
@@ -32,25 +32,47 @@ module.exports = (bot) => {
     }
     try {
       const data = JSON.parse(body);
-      bot.send({ room: "開発" }, asmMsg(data.Report));
+      const msg = asmMsg(data.Report);
+      if (msg) {
+        bot.send({ room: "開発" }, msg);
+      }
     } catch (e) {
       console.error(e);
       return;
     }
   };
 
-  const asmMsg = (info) => {
-    const attachments = [];
-    switch (info.Head.InfoKind) {
+  const asmMsg = (report) => {
+    if (report.Control.Status !== "通常") {
+      console.error("通常 では無い情報、スキップしました " + report.Control.Status);
+    }
+    let attachments = {
+      author_name: report.Head.Title,
+      color: "#FF4B00",
+      footer: `${report.Control.PublishingOffice} ${report.Head.InfoType}`,
+      ts: Math.floor(new Date(report.Head.ReportDateTime).getTime() / 1000)
+    }
+    switch (report.Head.InfoKind) {
+      case "震度速報":
+        attachments = message.MaxInt(report);
+        break;
+      case "震源速報":
+        attachments = message.Hypocenter(report);
+        break;
+      case "地震情報":
+        attachments = message.Earthquake(report);
+        break;
+      case "噴火速報":
+        attachments = message.Eruption(report);
+        break;
+      case "噴火に関する火山観測報":
+        attachments = message.Volcano(report);
+        break;
       default:
-        const headline = info.Head.Headline.Text;
-        attachments.push({
-          text: `${headline ? headline : info.Head.InfoKind}`,
-          color: '#2196F3'
-        });
+        return;
     }
     return {
-      attachments
+      attachments: [attachments]
     };
   };
 };
---
+++ scripts/message.js
@@ -0,0 +1,21 @@
+"use strict";
+exports.MaxInt = (attachments, report) => {
+  attachments.text = report.Head.Headline.Text;
+  return attachments;
+};
+exports.Hypocenter = (attachments, report) => {
+  attachments.text = report.Head.Headline.Text;
+  return attachments;
+};
+exports.Earthquake = (attachments, report) => {
+  attachments.text = report.Head.Headline.Text;
+  return attachments;
+};
+exports.Eruption = (attachments, report) => {
+  attachments.text = report.Head.Headline.Text;
+  return attachments;
+};
+exports.Volcano = (attachments, report) => {
+  attachments.text = report.Head.Headline.Text;
+  return attachments;
+};
