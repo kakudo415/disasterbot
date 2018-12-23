b770c320ac1c29c7b35bc47236fca26c128d5a0a
Kakudo <kakudo415@gmail.com>
2018-12-23 11:30:55 +0900

    地震情報　愚直

--- scripts/message.js
+++ scripts/message.js
@@ -1,43 +1,57 @@
 "use strict";
 
 // 震度速報
-exports.MaxInt = (attachments, report) => {
-  attachments.text = report.Head.Headline.Text;
+exports.MaxInt = (attachments, Report) => {
+  attachments.text = Report.Head.Headline.Text;
   return attachments;
 };
 
 // 震源速報
-exports.Hypocenter = (attachments, report) => {
-  attachments.text = report.Head.Headline.Text;
+exports.Hypocenter = (attachments, Report) => {
+  attachments.text = Report.Head.Headline.Text;
   return attachments;
 };
 
 // 地震情報
-exports.Earthquake = (attachments, report) => {
+exports.Earthquake = (attachments, Report) => {
   let fields = [];
-  console.log(typeof report.Body.Earthquake.Hypocenter.Area["jmx_eb:Coordinate"], report.Body.Earthquake.Hypocenter.Area["jmx_eb:Coordinate"]);
   fields.push({
     title: "震央地",
-    value: report.Body.Earthquake.Hypocenter.Area.Name,
+    value: Report.Body.Earthquake.Hypocenter.Area.Name,
     short: true
   });
   fields.push({
     title: "深さ",
-    value: `${ISO6709(String(report.Body.Earthquake.Hypocenter.Area["jmx_eb:Coordinate"]))[3].replace("-", "")}km`,
+    value: ISO6709(Report.Body.Earthquake.Hypocenter.Area.Coordinate)[3].replace("-", "") + "km",
     short: true
+  });
+  fields.push({
+    title: "規模",
+    value: Report.Body.Earthquake.Magnitude,
+    short: true
+  });
+  fields.push({
+    title: "最大震度",
+    value: Report.Body.Intensity.MaxInt,
+    short: true
+  });
+  fields.push({
+    title: "その他",
+    value: Report.Body.Comments.ForecastCommment,
+    short: false
   });
   attachments.fields = fields;
   return attachments;
 };
 
 // 噴火速報
-exports.Eruption = (attachments, report) => {
-  attachments.text = report.Head.Headline.Text;
+exports.Eruption = (attachments, Report) => {
+  attachments.text = Report.Head.Headline.Text;
   return attachments;
 };
 
 // 噴火に関する火山観測報
-exports.Volcano = (attachments, report) => {
+exports.Volcano = (attachments, Report) => {
   let fields = [];
   fields.push({
     title: "場所",
@@ -54,8 +68,8 @@ exports.Volcano = (attachments, report) => {
 };
 
 // その他
-exports.Other = (attachments, report) => {
-  attachments.text = report.Head.Headline.Text ? report.Head.Headline.Text : report.Head.InfoKind;
+exports.Other = (attachments, Report) => {
+  attachments.text = Report.Head.Headline.Text ? Report.Head.Headline.Text : Report.Head.InfoKind;
   return attachments;
 };
 
