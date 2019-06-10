160d8319522f1be892b3daa9b0a0e704b79ed7f2
Kakudo <kakudo415@gmail.com>
2019-06-10 20:05:41 +0900

    add volcano obs

--- message.py
+++ message.py
@@ -1,19 +1,71 @@
-def make_attachment(rep):
+import re
+
+def make_attachment(data):
     return {
-        'author_name': rep['Head']['Title'],
+        'author_name': data['Report']['Head']['Title'],
         'color': '#FF4B00',
-        'footer': rep['Control']['PublishingOffice'] + ' ' + rep['Head']['InfoType']
+        'footer': data['Report']['Control']['PublishingOffice'] + ' ' + data['Report']['Head']['InfoType']
     }
 
-def seismic_bulletin(rep):
-    attachment = make_attachment(rep)
+def iso6709(src):
+    match = re.search(r'([\+\-][\w|\.]+)([\+\-][\w|\.]+)([\+\-][\w|\.]+)\/', src)
+    dest = []
+    dest.append(match.group(1))
+    dest.append(match.group(2))
+    dest.append(match.group(3))
+    return dest
+
+# 震度速報
+def seismic_bulletin(data):
+    attachment = make_attachment(data)
+    fields = []
+    attachment['fields'] = fields
     return [attachment]
 
-def other(rep):
-    attachment = make_attachment(rep)
-    if len(rep['Head']['Headline']['Text']) > 0:
-        attachment = rep['Head']['Headline']['Text']
+# 震源情報
+def epicenter_bulletin(data):
+    attachment = make_attachment(data)
+    fields = []
+    fields.append({
+        'title': '震央地',
+        'value': data['Report']['Earthquake']['Hypocenter']['Area']['Name'],
+        'short': True
+    })
+    fields.append({
+        'title': '深さ',
+        'value': iso6709(data['Report']['Body']['Earthquake']['Area']['Coordinate']),
+        'short': True
+    })
+    fields.append({
+        'title': 'その他',
+        'value': data['Report']['Body']['Comments']['ForecastComment']['Text'],
+        'short': False
+    })
+    attachment['fields'] = fields
+    return [attachment]
+
+# 噴火に関する火山観測報
+def volcano_observation(data):
+    attachment = make_attachment(data)
+    fields = []
+    fields.append({
+        'title': '場所',
+        'value': data['Report']['Body']['VolcanoInfo']['Item']['Areas']['Area']['Name'] + ' ' + data['Report']['Body']['VolcanoInfo']['Item']['Areas']['Area']['CraterName'],
+        'short': True
+    })
+    fields.append({
+        'title': '現象',
+        'value': data['Report']['Body']['VolcanoInfo']['Item']['Kind']['Name'],
+        'short': True
+    })
+    attachment['fields'] = fields
+    return [attachment]
+
+def other(data):
+    attachment = make_attachment(data)
+    if len(data['Report']['Head']['Headline']['Text']) > 0:
+        attachment['text'] = data['Report']['Head']['Headline']['Text']
     else:
-        attachment = rep['Head']['InfoKind']
+        attachment['text'] = data['Report']['Head']['InfoKind']
     return [attachment]
 
--- run.py
+++ run.py
@@ -1,58 +1,66 @@
+import os
 import time
+import json
+
 import redis
 import requests
+import slack
 
 import message as msg
 
 kvs = redis.Redis()
+client = slack.WebClient(token=os.environ['DISASTER_BOT_TOKEN'])
 
 def main():
     while True:
         uuids = new_uuids()
         for uuid in uuids:
-            report = info(uuid)
-            message = make_message(report)
+            data = info(uuid)
+            message = make_message(data)
             if len(message) == 0:
                 break
             send('#災害情報', message)
         time.sleep(10) # 10秒に一回ポーリング
 
 def new_uuids():
+    unchecked = []
     response = requests.get(api_uri())
     uuids = response.json()['UUID']
     for uuid in uuids:
         # チェック済みのUUIDは無視する
-        if kvs.get(kvs_key(uuid)) == 'CHECKED':
-            uuids.remove(uuid)
+        if kvs.get(kvs_key(uuid)) == None:
+            unchecked.append(uuid)
         # 未チェックの物をチェック済みにする(15分間)
         kvs.set(kvs_key(uuid), 'CHECKED')
-        kvs.expire(kvs_key, 60 * 15)
-    return uuids
+        kvs.expire(kvs_key(uuid), 60 * 15)
+    return unchecked
 
 def info(uuid):
     response = requests.get(api_uri('/json/' + uuid))
     return response.json()
 
-def make_message(report):
+def make_message(data):
     # 訓練などの情報を無視する
-    status = report['Control']['Status']
+    status = data['Report']['Control']['Status']
     if status != '通常':
         print('通常ではない情報、スキップしました => ' + status)
         return ''
     # 情報の種類に応じてメッセージを作成する
-    kind = report['Head']['InfoKind']
+    kind = data['Report']['Head']['InfoKind']
     if kind == '震度速報':
-        return msg.seismic_bulletin(report)
+        return msg.seismic_bulletin(data)
     if kind == '震源速報':
-        pass
+        return msg.epicenter_bulletin(data)
     if kind == '地震情報':
         pass
     if kind == '噴火速報':
         pass
     if kind == '噴火に関する火山観測報':
-        pass
-    print(format(json.dumps(msg.other(report), indent=2))) # For DEBUG
+        return volcano_observation(data)
     return ''
+
+def send(channel, message):
+    client.chat_postMessage(channel=channel, attachments=json.dumps(message))
 
 def api_uri(src='/'):
     return 'https://kakudo.app/kishow' + src
